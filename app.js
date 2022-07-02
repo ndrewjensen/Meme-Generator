7bdbc33538f2cedd3a4ce095a3226f9e0371c8cd
Andrew Jensen <andrewkjensen@gmail.com>
2022-07-01 21:43:54 -0700

    eod july 1

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ submit.addEventListener('click',function(e) {
   const picBottom = document.querySelector('input[name="picBottom"').value;
   const fontsize = document.querySelector('input[name="font-size"').value;
   const color = document.querySelector('input[name="color"').value;
-  const location = document.querySelector('input[name="text-location"').value
+  const upperlocation = document.querySelector('input[name="upper-text-location"').value
+  const lowerlocation = document.querySelector('input[name="lower-text-location"').value
   let size = "";
     if (document.getElementById('smallsize').checked) {
       size = 'small';
@@ -27,34 +28,47 @@ submit.addEventListener('click',function(e) {
     }
 
   //create upper text element
-  const upper = document.createElement('p')
+  const upper = document.createElement('p');
   upper.innerText = picTop;
-  upper.setAttribute('class', 'upper')
-  upper.setAttribute("style","font-size:"+fontsize+"px;color:"+color+";top:"+location+"px;")
+  upper.setAttribute('class', 'upper');
+  upper.setAttribute("style","font-size:"+fontsize+"px;color:"+color+";top:"+upperlocation+"%;")
 
    //create lower text element
-   const lower = document.createElement('p')
+   const lower = document.createElement('p');
    lower.innerText = picBottom;
-   lower.setAttribute('class', 'lower')
-   lower.setAttribute("style","font-size:"+fontsize+"px;color:"+color+";bottom:"+location+"px;")
+   lower.setAttribute('class', 'lower');
+   lower.setAttribute("style","font-size:"+fontsize+"px;color:"+color+";bottom:"+lowerlocation+"%;");
 
   //create image element
   const image = document.createElement('img');
-  image.setAttribute("class", size)
+  image.setAttribute("class", size);
   image.setAttribute("src",link);
-  image.setAttribute("alt","Meme Image from URL")
+  image.setAttribute("alt","Meme Image from URL");
+
+  // //add delete button image to meme
+  // const del = document.createElement('img');
+  // del.setAttribute("class", 'del');
+  // del.setAttribute('src', 'delete.png');
+  // del.setAttribute('width','30px');
+
 
   //create meme container
   const newMeme = document.createElement('div');
   newMeme.classList.add('meme');
+
   //newMeme.setAttribute("size",size);
   newMeme.prepend(image);
   newMeme.prepend(upper);
   newMeme.append(lower);
+  //newMeme.append(del);
 
 
 
   memeContainer.prepend(newMeme);
 
 })
+
+// document.addEventListener('mouseover', function(e){
+//   e.AT_TARGET
+//})
 //use classes to define whether text is on pic or above
